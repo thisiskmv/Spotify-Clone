03d6347b7443c36e72a414ff7a5cab81ca20bef4
Samarth <samarthbsacharya@gmail.com>
2023-04-18 19:03:37 +0530

    libraries added

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import { Button, Text } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button>hi</Button>
       <h1>Spotify Clone</h1>
     </div>
   );
