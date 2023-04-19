6af6c7b6af6c3115d4471cb5f5ab8923fbb782af
thisiskmv <thisiskmv@gmail.com>
2023-04-19 17:22:29 +0530

    login signup

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import { Button, Text } from '@chakra-ui/react';
 import './App.css';
 import { SpotifyHomepage } from './Pages/Home';
-
+import Login from './Login & Signup/Login';
+import SignUp from './Login & Signup/SignUp';
 function App() {
   return (
     <div className="App">
-    
-      <h1>Spotify Clone</h1>
-     
+      {/* <Login/>  */}
+      <SignUp/> 
     </div>
   );
 }
