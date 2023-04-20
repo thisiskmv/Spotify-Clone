62ac1fab3bf6ac01b39c9a5060bef075920dea44
unknown <govind.pasi94@gmail.com>
2023-04-20 19:06:32 +0530

    details page

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import { Button, Text } from '@chakra-ui/react';
 import './App.css';
 import { SpotifyHomepage } from './Pages/Home';
+import DetailsPage from './Pages/DetailsPage';
+import LikePage from './Pages/LikePage';
 
 function App() {
   return (
     <div className="App">
     
-      <h1>Spotify Clone</h1>
+      {/* <h1>Spotify Clone</h1> */}
+      <DetailsPage/>
+      {/* <LikePage/> */}
      
     </div>
   );
