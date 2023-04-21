23907d2233ad705f7c65daaa3749b49e57cf465c
Samarth <samarthbsacharya@gmail.com>
2023-04-21 23:17:01 +0530

    profile added

--- spotify/src/Pages/Home.jsx
+++ spotify/src/Pages/Home.jsx
@@ -12,6 +12,10 @@ import {
   Divider,
   Text,
   VStack,
+  Menu,
+  MenuItem,
+  MenuButton,
+  MenuList,
 } from "@chakra-ui/react";
 import GridMain from "./Grid";
 import Player from "./Player";
@@ -37,16 +41,32 @@ const SpotifyHomepage = () => {
             /> */}
           </Box>
           <Box>
-            <ButtonGroup spacing={2}>
-           
-            {/* <Avatar size='md' name='Dan Abrahmov' src='https://bit.ly/dan-abramov' /> */}
+          { Loggedin ?
+          <Menu  color='white' bg='rgba(40,40,40,255)'>
+          <MenuButton  >
+          <Avatar size='md' name='User' src='https://bit.ly/dan-abramov' />
+          </MenuButton>
+          <MenuList bg='rgba(40,40,40,255)' color="white" border='0px solid grey'>
+            <MenuItem color='white'bg='rgba(40,40,40,255)' >Account</MenuItem>
+            <MenuItem color='white' bg='rgba(40,40,40,255)'>Profile</MenuItem>
+            <MenuItem color='white' bg='rgba(40,40,40,255)'>Mark as Draft</MenuItem>
+            <MenuItem color='white' bg='rgba(40,40,40,255)'>Setting</MenuItem>
+            <Divider/>
+            <MenuItem color='white' bg='rgba(40,40,40,255)'>Log Out</MenuItem>
+          </MenuList>
+        </Menu>
+          
+           :
+           <ButtonGroup spacing={2}>
               <Button colorScheme="black" color="white" mr="2">
                 Sign Up
               </Button>
               <Button p={6} borderRadius="20px">
                 Log In
               </Button>
-            </ButtonGroup>
+            </ButtonGroup>}
+
+            
           </Box>
         </Flex>
       </Box>
